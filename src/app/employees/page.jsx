ee80117b2a4ba1da1b2abf0029fcd9ff5f7ecef4
naol86 <naol.kasinet@a2sv.org>
2024-07-05 04:29:43 +0300

    test

--- src/app/employees/page.jsx
+++ src/app/employees/page.jsx
@@ -4,10 +4,10 @@ import AddEmployee from './AddEmployee';
 export default function Page() {
   return (
     <div className='grid grid-cols-4'>
-      <div className='col-span-1'>
+      <div className='lg:col-span-1 col-span-4'>
         <AddEmployee />
       </div>
-      <div className='col-span-3'>
+      <div className='lg:col-span-3 col-span-4'>
         <Employees />
       </div>
     </div>
