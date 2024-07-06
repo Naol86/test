17dd3d075f26b4f172c3fb7621feb925c97df4c4
naol86 <naol.kasinet@a2sv.org>
2024-07-06 23:52:26 +0300

    test

--- src/app/employees/Employee.jsx
+++ src/app/employees/Employee.jsx
@@ -2,21 +2,23 @@ import Link from 'next/link';
 export default function Employee({ data }) {
   return (
     <li className='max-w-md flex justify-between gap-x-6 py-5 px-2 bg-gray-50 rounded-lg'>
-      <div className='flex gap-x-4'>
-        <img
-          className='h-12 w-12 flex-none rounded-full bg-gray-50'
-          src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-          alt=''
-        />
-        <div className='min-w-0 flex-auto'>
-          <p className='text-sm font-semibold leading-6 text-gray-900'>
-            {data.name}
-          </p>
-          <p className='mt-1 truncate text-xs leading-5 text-gray-500'>
-            employee Email
-          </p>
+      <Link href={`/employees/${data.id}`}>
+        <div className='flex gap-x-4'>
+          <img
+            className='h-12 w-12 flex-none rounded-full bg-gray-50'
+            src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+            alt=''
+          />
+          <div className='min-w-0 flex-auto'>
+            <p className='text-sm font-semibold leading-6 text-gray-900'>
+              {data.name}
+            </p>
+            <p className='mt-1 truncate text-xs leading-5 text-gray-500'>
+              employee Email
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className='lg grid-flow-col flex'>
         <div className='shrink-0 sm:flex sm:flex-col sm:items-end grid-cols-2 mx-1'>
           <p className='text-sm leading-6 text-gray-900'>
@@ -26,7 +28,7 @@ export default function Employee({ data }) {
         </div>
         <div className='grid space-y-1 mt-0'>
           <button className='font-medium text-indigo-600 hover:scale-105 border bg-gray-200 border-gray-200 rounded-xl px-3 py-1'>
-            <Link href={`/employees/${data.id}`}>edit</Link>
+            <Link href={`/employees/add/${data.id}`}>edit</Link>
           </button>
           <button className='font-semibold text-gray-100 border-red-600 rounded-xl px-3 py-1 bg-red-600 border'>
             delete
--- src/app/positions/page.jsx
+++ src/app/positions/page.jsx
@@ -2,7 +2,10 @@ import Positions from './Positions';
 
 export default function PositionsPage() {
   return (
-    <div className=''>
+    <div className='text-center p-2'>
+      <div className='mx-auto font-bold text-gray-500 text-xl  inline rounded-lg'>
+        Positions
+      </div>
       <Positions />
     </div>
   );
